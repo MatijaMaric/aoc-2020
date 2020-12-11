aa0e0534b6ceff1c1bb56b37ba32d872e24612f7
Matija Maric <sirSlani@gmail.com>
2020-12-11 08:28:04 +0100

    day11 v2

--- day11/main.go
+++ day11/main.go
@@ -14,102 +14,25 @@ func main() {
 }
 
 func part1(input string) int {
-	lines := utils.SplitLines(input)
-	height := len(lines)
-	width := len(lines[0])
-	grid := make([][]rune, height)
-	for i, line := range lines {
-		grid[i] = make([]rune, width)
-		for j, c := range line {
-			grid[i][j] = c
-		}
-	}
-
-	changed := true
-	for changed {
-		changed = false
-		newGrid := make([][]rune, height)
-		for i := 0; i < height; i++ {
-			newGrid[i] = make([]rune, width)
-			for j := 0; j < width; j++ {
-				if grid[i][j] == '.' {
-					newGrid[i][j] = '.'
-				} else {
-					occupied := 0
-					for x := -1; x <= 1; x++ {
-						for y := -1; y <= 1; y++ {
-							if x == 0 && y == 0 {
-								continue
-							}
-							if i+x < 0 || j+y < 0 || i+x >= height || j+y >= width {
-								continue
-							}
-							if grid[i+x][j+y] == '#' {
-								occupied++
-							}
-						}
-					}
-					if occupied == 0 && grid[i][j] == 'L' {
-						newGrid[i][j] = '#'
-						changed = true
-					} else if occupied >= 4 && grid[i][j] == '#' {
-						newGrid[i][j] = 'L'
-						changed = true
-					} else {
-						newGrid[i][j] = grid[i][j]
-					}
-				}
-			}
-		}
-		grid = newGrid
-		// printGrid(grid)
-
-		// fmt.Println()
-	}
-
-	occupied := 0
-	for i := 0; i < height; i++ {
-		for j := 0; j < width; j++ {
-			if grid[i][j] == '#' {
-				occupied++
-			}
-		}
-	}
-
-	return occupied
-}
-
-func printGrid(grid [][]rune) {
-	for _, row := range grid {
-		for _, char := range row {
-			fmt.Printf("%c", char)
-		}
-		fmt.Println()
-	}
+	grid, _, _ := parseInput(input)
+	return solve(grid, 1, 4)
 }
 
 func part2(input string) int {
-	lines := utils.SplitLines(input)
-	height := len(lines)
-	width := len(lines[0])
-	grid := make([][]rune, height)
-	for i, line := range lines {
-		grid[i] = make([]rune, width)
-		for j, c := range line {
-			grid[i][j] = c
-		}
-	}
-	// printGrid(grid)
-
+	grid, width, height := parseInput(input)
 	minD := utils.Min(width, height)
 
+	return solve(grid, minD, 5)
+}
+
+func solve(grid [][]rune, minD int, maxOccupy int) int {
 	changed := true
 	for changed {
 		changed = false
-		newGrid := make([][]rune, height)
-		for i := 0; i < height; i++ {
-			newGrid[i] = make([]rune, width)
-			for j := 0; j < width; j++ {
+		newGrid := make([][]rune, len(grid))
+		for i := 0; i < len(grid); i++ {
+			newGrid[i] = make([]rune, len(grid[i]))
+			for j := 0; j < len(grid[i]); j++ {
 				if grid[i][j] == '.' {
 					newGrid[i][j] = '.'
 				} else {
@@ -119,18 +42,16 @@ func part2(input string) int {
 							if x == 0 && y == 0 {
 								continue
 							}
-							for mult := 1; mult < minD; mult++ {
+							for mult := 1; mult <= minD; mult++ {
 								nx := i + mult*x
 								ny := j + mult*y
-								if nx < 0 || ny < 0 || nx >= height || ny >= width {
+								if nx < 0 || ny < 0 || nx >= len(grid) || ny >= len(grid[i]) {
 									continue
 								}
-								if grid[nx][ny] == '.' {
-									continue
-								} else if grid[nx][ny] == '#' {
-									occupied++
-									break
-								} else {
+								if grid[nx][ny] != '.' {
+									if grid[nx][ny] == '#' {
+										occupied++
+									}
 									break
 								}
 							}
@@ -139,7 +60,7 @@ func part2(input string) int {
 					if occupied == 0 && grid[i][j] == 'L' {
 						newGrid[i][j] = '#'
 						changed = true
-					} else if occupied >= 5 && grid[i][j] == '#' {
+					} else if occupied >= maxOccupy && grid[i][j] == '#' {
 						newGrid[i][j] = 'L'
 						changed = true
 					} else {
@@ -149,12 +70,11 @@ func part2(input string) int {
 			}
 		}
 		grid = newGrid
-
 	}
 
 	occupied := 0
-	for i := 0; i < height; i++ {
-		for j := 0; j < width; j++ {
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
 			if grid[i][j] == '#' {
 				occupied++
 			}
@@ -162,4 +82,18 @@ func part2(input string) int {
 	}
 
 	return occupied
+}
+
+func parseInput(input string) (grid [][]rune, width, height int) {
+	lines := utils.SplitLines(input)
+	height = len(lines)
+	width = len(lines[0])
+	grid = make([][]rune, height)
+	for i, line := range lines {
+		grid[i] = make([]rune, width)
+		for j, c := range line {
+			grid[i][j] = c
+		}
+	}
+	return
 }
